0f0e00da7578d6d2e0578e4c5a15aa6a5c5adbf1
Taras Kostivi <taraskostsv@gmail.com>
2020-11-28 22:00:06 +0200

    firs search

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first;
+        System.out.println("Enter first number");
+        first = scanner.nextInt();
+
+        if (first >= 20 && first < 27){
+            System.out.println("You can do compulsory or contract service");
+        }else if(first >= 27 && first < 40){
+            System.out.println("You can only serve by contract");
+        }else if (first >= 40 && first < 50){
+            System.out.println("You can only serve by contract but you have risk");
+        }else if (first >= 60 && first < 100){
+            System.out.println("Most likely, you made a mistake and entered your age incorrectly.");
+        }else {
+            System.out.println("");
+        }
+    }
+}
